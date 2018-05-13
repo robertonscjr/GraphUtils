7183f89e4989ad09a52cefd7412deb98e7e82ff3
nicacioliveira <nicacio.sousa@ccc.ufcg.edu.br>
2018-05-12 22:57:17 -0300

    dfsimpl

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,10 +7,13 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		Graph<Integer> g = new Graph<>();
 		
-		g.readGraph("../GraphUtils/src/main/simpleGraph.txt");
+		//g.readGraph("../GraphUtils/src/main/simpleGraph.txt");
+		g.readGraph("../GraphUtils/src/main/simpleGraph2.txt");
 		//g.readWeightedGraph("../GraphUtils/src/main/weightedGraph.txt");
 		
-		System.out.println(g.graphRepresentation(RepresentationType.ADJACENCYMATRIX));
+		System.out.println(g.DFS(g.getVertex((long) 40)));
+		//System.out.println(g.graphRepresentation(RepresentationType.ADJACENCYLIST));
+		//System.out.println(g.graphRepresentation(RepresentationType.ADJACENCYMATRIX));
 	}
 
 }

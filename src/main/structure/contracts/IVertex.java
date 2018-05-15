0fb8f0b1f572b4857e83be31d74b1c554dcc5681
nicacioliveira <nicacio.sousa@ccc.ufcg.edu.br>
2018-05-14 21:15:29 -0300

    doc ivertex

--- src/main/structure/contracts/IVertex.java
+++ src/main/structure/contracts/IVertex.java
@@ -2,7 +2,29 @@ package main.structure.contracts;
 
 import main.structure.components.Edge;
 
+/**
+ * Ivertex
+ *
+ * @param <T>
+ */
 public interface IVertex<T> {
+	
+	/**
+	 * 
+	 * @param edge
+	 */
 	void addEdge(Edge<T> edge);
+	
+	/**
+	 * 
+	 * @param edge
+	 */
 	void removeEdge(Edge<T> edge);
+	
+	/**
+	 * Compares the distance of another vertex with the current one.
+	 * @param distanceValue
+	 * @return
+	 */
+	Boolean relaxDistance(double distanceValue);
 }
